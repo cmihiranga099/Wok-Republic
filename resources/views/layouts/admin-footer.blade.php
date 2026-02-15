92235ab5acce45dbf77f1b4c41d9e1123168bd0c
cmihiranga099 <>
2026-02-15 23:42:19 +0530

    craete blades

---
+++ resources/views/layouts/admin-footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-gray-800 text-white shadow mt-10">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <p class="text-center text-gray-400">
+            &copy; {{ date('Y') }} Wok Republic Admin. All rights reserved.
+        </p>
+    </div>
+</footer>
---
+++ resources/views/layouts/frontend-footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-gray-800 text-white shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <p class="text-center text-gray-400">
+            &copy; {{ date('Y') }} Wok Republic. All rights reserved.
+        </p>
+    </div>
+</footer>
